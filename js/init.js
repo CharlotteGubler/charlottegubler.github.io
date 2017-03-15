3db5eca0cd0f1e9c4083415878e79a272273cf02
CharlottGubler <charlotte@kaywa.com>
2017-03-14 23:27:55 -0700

    Nav Button
    deleted nav button in javascript that appeared on mobile

--- js/init.js
+++ js/init.js
@@ -12,7 +12,7 @@
 			global: { href: 'css/style.css', containers: 1400, grid: { gutters: ['2em', 0] } },
 			xlarge: { media: '(max-width: 1680px)', href: 'css/style-xlarge.css', containers: 1200 },
 			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', containers: 960, grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 980px)', href: 'css/style-medium.css', containers: '90%', grid: { zoom: 2 } },
+			medium: { median: '(max-width: 980px)', href: 'css/style-medium.css', containers: '90%', grid: { zoom: 2 } },
 			small: { media: '(max-width: 736px)', href: 'css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0], zoom: 3 } },
 			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css' }
 		},
@@ -24,15 +24,7 @@
 						transformTest: function() { return skel.vars.isMobile; }
 					},
 				
-				// Navigation Button.
-					navButton: {
-						breakpoints: 'medium',
-						height: '4em',
-						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
-						position: 'top-left',
-						side: 'top',
-						width: '6em'
-					},
+				
 
 				// Navigation Panel.
 					navPanel: {
